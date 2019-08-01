17b16807c6ab0d811668970c1380d3a8314c2f07
Thomas Pisula <tompisula@gmail.com>
2019-08-01 12:32:53 -0500

    Add project files.

---
+++ AspNetCoreBlazorJwtAuth.Client/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using AspNetCoreBlazorJwtAuth.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreBlazorJwtAuth.Client
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> Login(LoginModel loginModel);
+        Task Logout();
+        Task<RegisterResult> Register(RegisterModel registerModel);
+    }
+}
---
+++ AspNetCoreBlazorJwtAuth.Client/Startup.cs
@@ -0,0 +1,23 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreBlazorJwtAuth.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBlazoredLocalStorage();
+            services.AddAuthorizationCore();
+            services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+            services.AddScoped<IAuthService, AuthService>();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
